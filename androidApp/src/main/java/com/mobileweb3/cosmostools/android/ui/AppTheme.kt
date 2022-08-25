ebc3e89dd060f5654ea93a2fb25a2c82df45bbc2
Dmitriy Kuzmin <hook23@mail.ru>
2022-08-25 20:37:07 +0300

    android: update app theme

--- androidApp/src/main/java/com/mobileweb3/cosmostools/android/ui/AppTheme.kt
+++ androidApp/src/main/java/com/mobileweb3/cosmostools/android/ui/AppTheme.kt
@@ -1,28 +1,18 @@
 package com.mobileweb3.cosmostools.android.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val Orange = Color(0xfff8873c)
-private val Purple = Color(0xff6b70fc)
-private val LightColors = lightColors(
-    primary = Orange,
-    primaryVariant = Orange,
-    onPrimary = Color.White,
-    secondary = Purple,
-    onSecondary = Color.White
-)
+private val PrimaryColor = Color(0xff735095)
+
 private val DarkColors = darkColors(
-    primary = Orange,
-    primaryVariant = Orange,
-    onPrimary = Color.White,
-    secondary = Purple,
-    onSecondary = Color.White
+    primary = PrimaryColor,
+    onSecondary = Color.White,
+    background = Color.Black,
+    onPrimary = Color.White
 )
 
 @Composable
@@ -30,7 +20,7 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = if (isSystemInDarkTheme()) DarkColors else LightColors,
+        colors = DarkColors,
         content = {
             Surface(content = content)
         }

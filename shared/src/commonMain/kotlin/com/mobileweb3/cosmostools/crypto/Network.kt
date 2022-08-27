56f01f62e655848e13bfd19cf3b83fc76a3411fe
Dmitriy Kuzmin <hook23@mail.ru>
2022-08-27 14:16:47 +0300

    shared: add network entity and mock networks list

---
+++ shared/src/commonMain/kotlin/com/mobileweb3/cosmostools/crypto/Network.kt
@@ -0,0 +1,61 @@
+package com.mobileweb3.cosmostools.crypto
+
+data class Network(
+    val chain_name: String,
+    val status: String,
+    val website: String? = null,
+    val pretty_name: String,
+    val bech32_prefix: String,
+    val key_algos: List<String>,
+    val slip44: Int,
+    val fees: NetworkFees? = null,
+    val staking: NetworkStaking? = null,
+    val logo_URIs: NetworkLogos? = null,
+    val explorers: List<NetworkExplorer>? = null,
+    val assets: List<NetworkAsset>
+) {
+
+    fun getLogo(): String? {
+        return assets[0].logo_URIs?.png ?: assets[0].logo_URIs?.svg//logo_URIs?.png ?: logo_URIs?.svg
+    }
+}
+
+data class NetworkFees(
+    val fee_tokens: List<NetworkToken>? = null
+)
+
+data class NetworkStaking(
+    val staking_tokens: List<NetworkToken>? = null
+)
+
+data class NetworkToken(
+    val denom: String,
+    val fixed_min_gas_price: Double? = null,
+    val low_gas_price: Double? = null,
+    val average_gas_price: Double? = null,
+    val high_gas_price: Double? = null,
+    val exponent: Int? = null
+)
+
+data class NetworkLogos(
+    val png: String? = null,
+    val svg: String? = null
+)
+
+data class NetworkExplorer(
+    val kind: String, //mintscan
+    val url: String, //https://www.mintscan.io/osmosis
+    val tx_page: String? = null, //https://www.mintscan.io/osmosis/txs/${txHash}
+    val account_page: String? = null //https://www.mintscan.io/osmosis/account/${accountAddress}
+)
+
+data class NetworkAsset(
+    val description: String? = null,
+    val denom_units: List<NetworkToken>,
+    val base: String,
+    val name: String,
+    val display: String,
+    val symbol: String,
+    val logo_URIs: NetworkLogos?,
+    val coingecko_id: String
+)
---
+++ shared/src/commonMain/kotlin/com/mobileweb3/cosmostools/crypto/mockNetworks.kt
@@ -0,0 +1,136 @@
+package com.mobileweb3.cosmostools.crypto
+
+val mockNetworks = listOf(
+    Network(
+        chain_name = "cosmoshub",
+        status = "Live",
+        website = null,
+        pretty_name = "Cosmos Hub",
+        bech32_prefix = "cosmos",
+        key_algos = listOf("secp256k1"),
+        slip44 = 118,
+        fees = NetworkFees(
+            fee_tokens = listOf(
+                NetworkToken(
+                    denom = "uatom",
+                    fixed_min_gas_price = 0.0
+                )
+            )
+        ),
+        logo_URIs = null,
+        explorers = listOf(
+            NetworkExplorer(
+                kind = "mintscan",
+                url = "https://www.mintscan.io/cosmos",
+                tx_page = "https://www.mintscan.io/cosmos/txs/"
+            )
+        ),
+        assets = listOf(
+            NetworkAsset(
+                description = "The native staking and governance token of the Cosmos Hub.",
+                denom_units = listOf(
+                    NetworkToken(
+                        denom = "uatom",
+                        exponent = 0
+                    ),
+                    NetworkToken(
+                        denom = "atom",
+                        exponent = 6
+                    )
+                ),
+                base = "uatom",
+                name = "Cosmos",
+                display = "atom",
+                symbol = "ATOM",
+                logo_URIs = NetworkLogos(
+                    png = "https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/images/atom.png",
+                    svg = "https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/images/atom.svg"
+                ),
+                coingecko_id = "cosmos"
+            )
+        )
+    ),
+    Network(
+        chain_name = "osmosis",
+        status = "live",
+        website = null,
+        pretty_name = "Osmosis",
+        bech32_prefix = "osmo",
+        key_algos = listOf("secp256k1"),
+        slip44 = 118,
+        fees = NetworkFees(
+            fee_tokens = listOf(
+                NetworkToken(
+                    denom = "uosmo",
+                    fixed_min_gas_price = 0.0,
+                    low_gas_price = 0.0,
+                    average_gas_price = 0.025,
+                    high_gas_price = 0.04
+                )
+            ),
+        ),
+        staking = NetworkStaking(
+            staking_tokens = listOf(
+                NetworkToken(
+                    denom = "uosmo"
+                )
+            )
+        ),
+        logo_URIs = NetworkLogos(
+            png = "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmosis-chain-logo.png"
+        ),
+        explorers = listOf(
+            NetworkExplorer(
+                kind = "mintscan",
+                url = "https://www.mintscan.io/osmosis",
+                tx_page = "https://www.mintscan.io/osmosis/txs/",
+                account_page = "https://www.mintscan.io/osmosis/account/"
+            )
+        ),
+        assets = listOf(
+            NetworkAsset(
+                description = "The native token of Osmosis",
+                denom_units = listOf(
+                    NetworkToken(
+                        denom = "uosmo",
+                        exponent = 0
+                    ),
+                    NetworkToken(
+                        denom = "osmo",
+                        exponent = 6
+                    ),
+                ),
+                base = "uosmo",
+                name = "Osmosis",
+                display = "osmo",
+                symbol = "OSMO",
+                logo_URIs = NetworkLogos(
+                    png = "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.png",
+                    svg = "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.svg"
+                ),
+                coingecko_id = "osmosis"
+            ),
+            NetworkAsset(
+                denom_units = listOf(
+                    NetworkToken(
+                        denom = "uion",
+                        exponent = 0
+                    ),
+                    NetworkToken(
+                        denom = "ion",
+                        exponent = 6
+                    )
+                ),
+                base = "uion",
+                name = "Ion",
+                display = "ion",
+                symbol = "ION",
+                logo_URIs = NetworkLogos(
+                    png = "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/ion.png",
+                    svg = "https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/ion.svg"
+                ),
+                coingecko_id = "ion"
+            )
+        )
+    )
+)

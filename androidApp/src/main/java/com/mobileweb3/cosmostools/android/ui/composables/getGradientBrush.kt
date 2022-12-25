5d4c72804765f8bafc0f7469279dae29f4658d39
Dmitriy Kuzmin <hook23@mail.ru>
2022-12-25 08:55:13 +0300

    fix crash of building brush

--- androidApp/src/main/java/com/mobileweb3/cosmostools/android/ui/composables/getGradientBrush.kt
+++ androidApp/src/main/java/com/mobileweb3/cosmostools/android/ui/composables/getGradientBrush.kt
@@ -7,46 +7,57 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.palette.graphics.Palette
 
+private val DEFAULT_GRADIENT = Brush.linearGradient(listOf(Color.Black, Color.Black))
 @Composable
 fun getGradientBrush(palette: Palette?, reference: String?): Brush {
-    val sharedPreferences = LocalContext.current.getSharedPreferences("brush", MODE_PRIVATE)
-    val savedColorsString = sharedPreferences.getString(reference, null)
+    runCatching {
+        val sharedPreferences = LocalContext.current.getSharedPreferences("brush", MODE_PRIVATE)
+        val savedColorsString = sharedPreferences.getString(reference, null)
 
-    if (savedColorsString != null) {
-        val savedColorsArray = savedColorsString.split(" ")
-        val savedColors = savedColorsArray.map {
-            Color(it.toInt())
+        if (savedColorsString != null) {
+            val savedColorsArray = savedColorsString.split(" ")
+            val savedColors = savedColorsArray.map {
+                Color(it.toInt())
+            }
+            return if (savedColors.size == 1) {
+                Brush.linearGradient(listOf(savedColors[0], savedColors[0]))
+            } else {
+                Brush.linearGradient(savedColors)
+            }
         }
-        return if (savedColors.size == 1) {
-            Brush.linearGradient(listOf(savedColors[0], savedColors[0]))
-        } else {
-            Brush.linearGradient(savedColors)
-        }
+
+        return palette?.let { safePalette ->
+            val mostThreeColors = safePalette.swatches
+                .sortedByDescending { it.population }
+                .take(3)
+
+            val colorsAsString = when (mostThreeColors.size) {
+                1 -> {
+                    "${mostThreeColors[0].rgb}"
+                }
+                2 -> {
+                    "${mostThreeColors[0].rgb} ${mostThreeColors[1].rgb}}"
+                }
+                3 -> {
+                    "${mostThreeColors[0].rgb} ${mostThreeColors[1].rgb} ${mostThreeColors[2].rgb}"
+                }
+                else -> null
+            }
+
+            sharedPreferences
+                .edit()
+                .putString(reference, colorsAsString)
+                .apply()
+
+            return when (mostThreeColors.size) {
+                0 -> DEFAULT_GRADIENT
+                1 -> Brush.linearGradient(listOf(Color(mostThreeColors[0].rgb), Color(mostThreeColors[0].rgb)))
+                else -> Brush.linearGradient(mostThreeColors.map { Color(it.rgb) })
+            }
+        } ?: DEFAULT_GRADIENT
+    }.onFailure {
+        return DEFAULT_GRADIENT
     }
 
-    return palette?.let { safePalette ->
-        val mostThreeColors = safePalette.swatches
-            .sortedByDescending { it.population }
-            .take(3)
-
-        val colorsAsString = when (mostThreeColors.size) {
-            1 -> {
-                "${mostThreeColors[0].rgb}"
-            }
-            2 -> {
-                "${mostThreeColors[0].rgb} ${mostThreeColors[1].rgb}}"
-            }
-            3 -> {
-                "${mostThreeColors[0].rgb} ${mostThreeColors[1].rgb} ${mostThreeColors[2].rgb}"
-            }
-            else -> null
-        }
-
-        sharedPreferences
-            .edit()
-            .putString(reference, colorsAsString)
-            .apply()
-
-        return@let Brush.linearGradient(mostThreeColors.map { Color(it.rgb) })
-    } ?: Brush.linearGradient(listOf(Color.Black, Color.Black))
+    return DEFAULT_GRADIENT
 }
